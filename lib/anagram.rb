3bacc5b87fc5a7a03c4a8b33c51b73d47a945246
Victor Plushko <vicplushko33@gmail.com>
2020-04-15 05:36:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  anagrams = []
+ 
+  def initialize(word)
+    @word = word
+   end
+   
+   def match(word)
+     if anagrams.find do |words|
+       words != word
+       return []
+    end
+   end
+  end
+ end
